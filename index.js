0dfefa3900add7136b4761c254aa57db80072a59
neriheredia <developer.heredia@gmail.com>
2022-03-07 12:11:07 -0300

    Solution Socket.io

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //EXPRESS para el server
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8300
+
+const http = require('http')
+
+const server = http.createServer(app)
 
 //Config
 const dotenv = require('dotenv')
@@ -25,7 +28,62 @@ mongoose.connect(
 app.use('/api', allRoutes)
 
 
+//CONFIG SOCKET IO
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "https://front-chat-test.vercel.app/"
+    }
+})
+
+
+//SOCKET
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some(user => user.userId === userId) &&
+        users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+    //when ceonnect
+    console.log("a user connected.");
+
+    // take userId and socketId from user
+    socket.on("addUser", (userId) => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
+
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text,
+        });
+    });
+
+    // console.log("USERS:", users);
+
+    //when disconnect
+    socket.on("disconnect", () => {
+        console.log("a user disconnected!");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    });
+});
+
+
+
 //POR ONLINE
-app.listen(port, () => {
-    console.log(`Server On Port ${port}`);
+server.listen(process.env.PORT || 8300, () => {
+    console.log(`Server On Port 8300`);
 })
